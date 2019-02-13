ac9a03a3dd7b77c2fbc2ce842b646f34580e6bf3
Victor Ozoh <bbgenie@gmail.com>
2019-02-13 07:06:43 -0600

    edits to ekf node

--- src/ekf.py
+++ src/ekf.py
@@ -28,40 +28,52 @@ def ExtendedKalmanFilter(z, vx, vy, angular_vel, dt):
     vel = np.array([linear_vel, angular_vel ])
     # x' from motion/ prediction
     old_x = x
-    x = x + np.dot(f, vel)
+    old_theta = x[2]
+    x_prime = x + np.dot(f, vel)
     # use F = Jacobian(f) to update covariance
     F = np.array([[1, 0, -1*vel*dt*math.sin(theta)],[0, 1, vel*dt*math.cos(theta)],[0, 0, 1]])
-    P = np.dot(F,P).dot(F.T) + Q
+    P_prime = np.dot(F,P).dot(F.T) + Q
 
     # update
-    # innovation
     dth = angular_vel * dt
+    dtrans = math.sqrt((x_prime[0] - old_x[0])**2 + (x_prime[1] - old_x[1])**2)
+    h = np.array([[math.cos(old_theta + dth/2), 0],[math.sin(old_theta + dth/2), 0],[0, 1]])
+    disp = np.array([dtrans, dth]) # displacement
+    pred_z = old_x + np.dot(h,disp)
+    # innovation
+    innovation = z - pred_z
+    H = np.array([[1, 0, -1* dtrans*math.sin(old_theta + dth/2)],[0, 1, dtrans*math.cos(old_theta + dth/2)],[0, 0, 1]])
+    innovation_covariance = np.dot(H,P_prime).dot(H.T) + R
+    #  compute Kalman gain
+    kalman_gain = np.dot(P_prime, H.T).dot(np.linalg.inv(innovation_covariance))
 
-    h = np.array([[math.cos(theta + dth/2), 0],[math.sin(theta + dth/2), 0],[0, 1]])
-    pred_z = old_x + np.dot(h,)
-    y = z - pred_z
-
+    x = x + np.dot(kalman_gain, innovation)
+    P = P_prime - np.dot(kalman_gain,H).dot(P_prime)
     return x, P
 
 
 def callback(msg):
     # get current measurements
-    x = msg.pose.pose.position.x
-    y = msg.pose.pose.position.x
+    x_pos = msg.pose.pose.position.x
+    y_pos = msg.pose.pose.position.x
     vx = msg.twist.twist.linear.x
     vy = msg.twist.twist.linear.y
     angular_vel = msg.twist.twist.angular.z
     quaternion = msg.pose.pose.orientation
     euler = tf.transformations.euler_from_quaternion(quaternion)
     th = euler[2]
-    z = np.array([x, y, th]) #actual measurement
+    z = np.array([x_pos, y_pos, th]) #actual measurement
     # call extended kalman filter function
-    filtered = Odometry()
+    filtered_msg = msg
     dt = rospy.Time.now().to_sec() - last_time # delta time
-    filtered = ExtendedKalmanFilter(z, vx, vy, angular_vel, dt)
+    new_state, new_cov = ExtendedKalmanFilter(z, vx, vy, angular_vel, dt)
 
+    filtered_msg.pose.pose.position.x = new_state[0]
+    filtered_msg.pose.pose.position.y = new_state[1]
+    euler[2] = new_state[2]
+    filtered_msg.pose.pose.orientation = tf.transformations.quaternion_from_euler(euler[0], euler[1], euler[2])
     # publish filtered message
-    pub.publish(filtered)
+    pub.publish(filtered_msg)
 
 if __name__ == "__main__":
     try:
